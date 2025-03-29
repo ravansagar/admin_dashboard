1d7921f61d6f160b0996faa7ca8ff09dd0ac18fd
Sagar Thakur <huntersagar08@gmail.com>
2025-03-29 17:27:39 +0545

    put param def changed

--- src/app/api/categories/[categoryId]/route.ts
+++ src/app/api/categories/[categoryId]/route.ts
@@ -1,16 +1,26 @@
 import { NextResponse } from "next/server";
 import { updateCategory } from "@/server/actions";
 
-export async function PUT(request: Request, { params }: { params: { categoryId: string } }) {
-    const categoryId = parseInt(params.categoryId, 10);
+export async function PUT(request: Request, context: { params: { categoryId: string } }) {
+    const { categoryId } = context.params;
+
+    const parsedCategoryId = parseInt(categoryId, 10);
     const { name, imageUrl } = await request.json();
+
     if (!name || !imageUrl) {
-        return NextResponse.json({ error: "Name and Image URL are required." }, { status: 400 });
+        return NextResponse.json(
+            { error: "Name and Image URL are required." },
+            { status: 400 }
+        );
     }
+
     try {
-        const newCategory = await updateCategory(categoryId, { name, imageUrl });
+        const newCategory = await updateCategory(parsedCategoryId, { name, imageUrl });
         return NextResponse.json(newCategory, { status: 200 });
     } catch {
-        return NextResponse.json({ error: "Something went wrong while updating the category." }, { status: 500 });
+        return NextResponse.json(
+            { error: "Something went wrong while updating the category." },
+            { status: 500 }
+        );
     }
 }
