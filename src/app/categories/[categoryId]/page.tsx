165a1484707d28613a751abeed9b4e47dd2dccbb
Sagar Thakur <huntersagar08@gmail.com>
2025-03-29 18:42:57 +0545

    abc

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -1,21 +1,19 @@
 import DashboardShell from "@/components/dashboardShell";
 import DashboardHeader from "@/components/dashboardHeader";
 import { CategoryForm } from "@/components/categoryForm";
-import { getCategory } from "@/server/actions";
 
 interface CategoryPageProps {
-    params: { categoryId: string; }
+  params: { categoryId: string };
 }
 
-export default async function editCategoryPage({params}: CategoryPageProps){
-    const categoryIdNumber = Number(params.categoryId);
-    const Category = await getCategory(categoryIdNumber);
-    return (
-        <DashboardShell>
-            <DashboardHeader heading="Edit Category" text="Update category details" />
-            <div className="grid gap-8">
-                <CategoryForm category={Category} />
-            </div>
-        </DashboardShell>
-    );
+export default function editCategoryPage({ params }: CategoryPageProps) {
+  const categoryIdNumber = Number((params as { categoryId: string }).categoryId); // Type assertion
+  return (
+    <DashboardShell>
+      <DashboardHeader heading="Edit Category" text="Update category details" />
+      <div className="grid gap-8">
+        <CategoryForm category={{ id: categoryIdNumber, name: '', imageUrl: '' }} />
+      </div>
+    </DashboardShell>
+  );
 }
