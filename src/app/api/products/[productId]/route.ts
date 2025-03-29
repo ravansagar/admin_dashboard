4376ef457f035b66e62402e784664adf25072421
Sagar Thakur <huntersagar08@gmail.com>
2025-03-29 18:13:20 +0545

    same problem

--- src/app/api/products/[productId]/route.ts
+++ src/app/api/products/[productId]/route.ts
@@ -1,17 +1,21 @@
 import { NextResponse } from "next/server";
 import { updateProduct } from "@/server/actions";
 
-export async function PUT(request: Request, { params }: { params: { productId: string } }) {
+export async function PUT(req: Request) {
+    const url = new URL(req.url);
+    const productId = url.pathname.split('/').pop() || "0";
 
-    const { name, price, description, imageUrl, initialStock, availableStock, categoryId } = await request.json();
-
-    const productId = Number(params.productId);
+    const parsedProductId = parseInt(productId, 10);
+    if (isNaN(parsedProductId)) {
+        return NextResponse.json({ error: 'Invalid categoryId.' }, { status: 400 });
+    }
+    const { name, price, description, imageUrl, initialStock, availableStock, categoryId } = await req.json();
 
     if (!name || !price || !description || !imageUrl || !initialStock || !availableStock || !categoryId) {
         return NextResponse.json({ error: "Data are missing to update the product." }, { status: 400 });
     }
     try {
-        const newProduct = await updateProduct( productId, { name, price, description, imageUrl, initialStock, availableStock, categoryId }); 
+        const newProduct = await updateProduct( parsedProductId, { name, price, description, imageUrl, initialStock, availableStock, categoryId }); 
         return NextResponse.json(newProduct, { status: 200 });
     } catch {
         return NextResponse.json({ error: "Something went wrong while updating product." }, { status: 500 });
