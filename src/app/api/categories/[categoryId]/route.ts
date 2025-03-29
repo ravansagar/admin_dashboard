fd38715038751d471038f7af5f897077f0430492
Sagar Thakur <huntersagar08@gmail.com>
2025-03-29 17:56:02 +0545

    same error

--- src/app/api/categories/[categoryId]/route.ts
+++ src/app/api/categories/[categoryId]/route.ts
@@ -1,28 +1,24 @@
-import { NextResponse } from "next/server";
-import { updateCategory } from "@/server/actions";
+import { NextResponse } from 'next/server';
+import { updateCategory } from '@/server/actions';
+import { NextRequest } from 'next/server'; 
 
-export async function PUT(request: Request, { params }: { params: { categoryId: string } }) {
-    const categoryId = parseInt(params.categoryId, 10);
-
-    if (isNaN(categoryId)) {
-        return NextResponse.json({ error: "Invalid categoryId." }, { status: 400 });
+export async function PUT(req: NextRequest, context: { params: { categoryId: string } }) {
+    const { categoryId } = context.params; 
+    const parsedCategoryId = parseInt(categoryId, 10);
+    if (isNaN(parsedCategoryId)) {
+        return NextResponse.json({ error: 'Invalid categoryId.' }, { status: 400 });
     }
-    const { name, imageUrl } = await request.json();
+
+    const { name, imageUrl } = await req.json();
 
     if (!name || !imageUrl) {
-        return NextResponse.json(
-            { error: "Name and Image URL are required." },
-            { status: 400 }
-        );
+        return NextResponse.json({ error: 'Name and Image URL are required.' }, { status: 400 });
     }
 
     try {
-        const newCategory = await updateCategory(categoryId, { name, imageUrl });
+        const newCategory = await updateCategory(parsedCategoryId, { name, imageUrl });
         return NextResponse.json(newCategory, { status: 200 });
-    } catch {
-        return NextResponse.json(
-            { error: "Something went wrong while updating the category." },
-            { status: 500 }
-        );
+    } catch (error) {
+        return NextResponse.json({ error: 'Something went wrong while updating the category.' }, { status: 500 });
     }
 }
