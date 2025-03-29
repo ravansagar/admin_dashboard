16f55ff8d38a211c46d19a2aa353f3a1285acc4a
Sagar Thakur <huntersagar08@gmail.com>
2025-03-29 22:20:56 +0545

    Some problems solved

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -1,23 +1,26 @@
+"use client"
+
 import DashboardShell from "@/components/dashboardShell";
 import DashboardHeader from "@/components/dashboardHeader";
 import { CategoryForm } from "@/components/categoryForm";
-import { NextResponse } from "next/server";
+import { useParams } from "next/navigation";
 
-const EditCategoryPage = (req: Request) => {
-    const url = new URL(req.url);
-    const categoryId = url.pathname.split('/').pop() || "0";
-    const categoryIdNumber = Number(categoryId);
+
+export default function EditCategoryPage() {
+
+    const params = useParams();
+    const categoryIdNumber = Number(params.categoryId);
+
     if (isNaN(categoryIdNumber)) {
-        return NextResponse.json({ error: 'Invalid categoryId.' }, { status: 400 });
+        return <p className="text-red-500">Invalid Category ID</p>;
     }
+
     return (
         <DashboardShell>
             <DashboardHeader heading="Edit Category" text="Update category details" />
             <div className="grid gap-8">
-                <CategoryForm category={{ id: categoryIdNumber, name: '', imageUrl: '' }} />
+                <CategoryForm category={{ id: categoryIdNumber, name: "", imageUrl: "" }} />
             </div>
         </DashboardShell>
     );
-};
-
-export default EditCategoryPage;
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,20 +1,17 @@
+"use client"
+
 import DashboardHeader from "@/components/dashboardHeader";
 import DashboardShell from "@/components/dashboardShell";
 import ProductForm from "@/components/productForm";
-import { NextResponse } from "next/server";
+import { useParams } from "next/navigation";
 
-export default function EditProductPage(req: Request) {
-    const url = new URL(req.url);
-    const productId = url.pathname.split('/').pop() || "0";
-    const productIdNumber = Number(productId);
-    if (isNaN(productIdNumber)) {
-        return NextResponse.json({ error: 'Invalid categoryId.' }, { status: 400 });
-    }
+export default function EditProductPage() {
+    const params = useParams();
     return (
         <DashboardShell>
             <DashboardHeader heading="Edit Product" text="Update product details" />
             <div className="grid gap-8">
-                <ProductForm product={{ id: productIdNumber, name: '', price: 0, description: '', imageUrl: '', categoryId: 0, initialStock: 0, availableStock: 0 }} />
+                <ProductForm product={{ id: Number(params.productId), name: '', price: 0, description:'', imageUrl: '', categoryId: 0, initialStock: 0, availableStock: 0}} />
             </div>
         </DashboardShell>
     );
