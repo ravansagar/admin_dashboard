07b415b8549a0e56b73d6d06bc3a0d1bbbe88fb6
Sagar Thakur <huntersagar08@gmail.com>
2025-03-30 16:37:11 +0545

    a

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {
+      config: './tailwind.config.js'
+    },
+    autoprefixer: {},
+  }
+}
